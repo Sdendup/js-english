9b4a402f69228383949b7adbc6cf4680f289e407
Sdendup <sdendup024@gmail.com>
2025-01-11 17:52:26 +0000

    array

---
+++ js2/array.js
@@ -0,0 +1,10 @@
+const myArr = [0,2,3,4,5,6,7,8,9] // array components are called elements
+
+const myfav=[ 'Sonam  number |"
+    1/3 undefined:any[]
+    ]
+
+// link:https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+
+console.log(myArr[3])
+
