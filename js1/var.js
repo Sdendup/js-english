48311d9746fae685597e4b58cd7afdaf4a8ebf9d
Sdendup <sdendup024@gmail.com>
2025-01-11 19:39:28 +0000

    variables

---
+++ js1/var.js
@@ -0,0 +1,14 @@
+const userID= 100
+
+let userName ='Sonam'
+let UserEmail='sd120@gmail.com'
+var userPassword = 12345678
+
+
+// Print comment in js
+
+console.log(userName)
+console.log([userID,userName,UserEmail, userPassword])
+
+// let is preferred over var in js 
+
