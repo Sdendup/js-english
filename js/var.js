d2a3243030719515fca4cdf56d2b1ad0ff04cc4f
Sdendup <sdendup024@gmail.com>
2025-01-11 14:22:33 +0000

    js code config

---
+++ js/var.js
@@ -0,0 +1,14 @@
+const userId = 102
+
+let userName='Sonam'
+let userEmail='sd10@google.com'
+var userPassword = 123456
+let userCity = 'Paro'
+let userState='Paro'
+
+
+// Print comment 
+
+//console.log(userName)
+console.log([userId,userName,userEmail,userPassword,userCity,userState])
+
