e921a9aa221828e37127c2a0a893eba70feba6f3
Sdendup <sdendup024@gmail.com>
2025-01-11 19:42:41 +0000

    variables

--- js1/var.js
+++ js1/var.js
@@ -1,5 +1,7 @@
-const userID= 100
+const userID= 100 // data in the variable cannot change 
 
+
+//date assigned can be change 
 let userName ='Sonam'
 let UserEmail='sd120@gmail.com'
 var userPassword = 12345678
@@ -12,3 +14,4 @@ console.log([userID,userName,UserEmail, userPassword])
 
 // let is preferred over var in js 
 
+// documentation link:https://developer.mozilla.org/en-US/docs/Learn_web_development/Core/Scripting/Variables
